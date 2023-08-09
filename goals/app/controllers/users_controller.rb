cde4de73bcdb330654f39208ce87144d9dab2dc9
Sanjid <dsanjid@gmail.com>
2023-08-09 15:07:38 -0400

    finished User controller and specs

--- goals/app/controllers/users_controller.rb
+++ goals/app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :password_digest, :session_token)
+      params.require(:user).permit(:username, :password)
     end
 end
